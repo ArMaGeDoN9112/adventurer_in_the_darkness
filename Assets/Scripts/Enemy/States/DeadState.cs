b2b885885b9a5138537512a1a15f1ea6e7b55bc6
ArMaG3D0N <ilyakur025@gmail.com>
2024-07-18 16:55:54 +0800

    Homework #4

---
+++ Assets/Scripts/Enemy/States/DeadState.cs
@@ -0,0 +1,47 @@
+using BHSCamp.FSM;
+using UnityEngine;
+
+namespace BHSCamp
+{
+    public class DeadState : FsmState
+    {
+        private PatrolEnemy _enemy;
+        private Animator _animator;
+        private bool _respawn;
+        private float _respawnTime;
+
+        public DeadState(Fsm fsm, PatrolEnemy enemy, bool respawn, float respawnTime) : base(fsm)
+        {
+            _respawn = respawn;
+            _respawnTime = respawnTime;
+            _animator = enemy.GetComponent<Animator>();
+            _enemy = enemy;
+        }
+
+        public override void Enter()
+        {
+            _animator.SetBool("IsDead", true);
+        }
+
+        // STEP 11: Если _respawn == true,
+        // через _respawnTime секунд мы должны выйти из состояния Dead
+        // и восстановить здоровье
+        public override void Update(float deltaTime)
+        {
+            if (_respawn)
+            {
+                _respawnTime -= Time.fixedDeltaTime;
+                if (_respawnTime <= 0)
+                    Fsm.SetState<PatrolState>();
+            }
+            return;
+        }
+
+        public override void Exit()
+        {
+            _animator.SetBool("IsDead", false);
+            Health health = _enemy.GetComponent<Health>();
+            health.Heal(health.MaxHealth);
+        }
+    }
+}
